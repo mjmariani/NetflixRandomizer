204c92a1c2c8f7b38092eaea8349ed1730c93d0b
Marlon J Mariani <marlon.j.mariani@gmail.com>
2021-01-24 16:50:27 -0500

    Add files via upload

--- NetflixRandomizer.sql
+++ NetflixRandomizer.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "Users" (
   "user_id" SERIAL UNIQUE PRIMARY KEY,
-  "first_name" text,
-  "last_name" text,
+  "first_name" string,
+  "last_name" string,
   "created_at" timestamp,
   "country_code" int,
   "region_code" int,
@@ -11,23 +11,24 @@ CREATE TABLE "Users" (
   "auth_id" int NOT NULL,
   "email" varchar,
   "gender_id" int NOT NULL,
-  "details" text,
-  "confirmation_code" text NOT NULL,
+  "details" string,
+  "confirmation_code" string NOT NULL,
   "confirmation_time" timestamp,
   "genres_liked_id" int
 );
 
 CREATE TABLE "Authentication" (
   "auth_id" int UNIQUE PRIMARY KEY,
-  "username" text NOT NULL,
-  "password" text NOT NULL,
+  "username" string NOT NULL,
+  "password_hash" string NOT NULL,
+  "password_salt" string NOT NULL,
   "created_at" timestamp NOT NULL,
   "city_id" int
 );
 
 CREATE TABLE "Country" (
   "id" int UNIQUE PRIMARY KEY,
-  "name" text NOT NULL,
+  "name" string NOT NULL,
   "city_id" int,
   "region_id" int,
   "state_id" int,
@@ -36,49 +37,36 @@ CREATE TABLE "Country" (
 
 CREATE TABLE "City" (
   "id" int UNIQUE PRIMARY KEY,
-  "name" text NOT NULL
+  "name" string NOT NULL
 );
 
 CREATE TABLE "Region" (
   "id" int UNIQUE PRIMARY KEY,
-  "name" text NOT NULL
+  "name" string NOT NULL
 );
 
 CREATE TABLE "State" (
   "id" int UNIQUE PRIMARY KEY,
-  "name" text NOT NULL
+  "name" string NOT NULL
 );
 
 CREATE TABLE "Province" (
   "id" int UNIQUE PRIMARY KEY,
-  "name" text NOT NULL
+  "name" string NOT NULL
 );
 
 CREATE TABLE "Movies" (
-  "movie_id" SERIAL PRIMARY KEY NOT NULL,
-  "name" text,
-  "details" text,
-  "date_released" date,
-  "runtime" int
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "name" string,
+  "API_used" string NOT NULL,
+  "API_id" string NOT NULL
 );
 
 CREATE TABLE "TV_Shows" (
   "id" SERIAL PRIMARY KEY NOT NULL,
-  "name" text,
-  "details" text,
-  "date_released" date,
-  "runtime_total" int,
-  "num_of_episodes" int
-);
-
-CREATE TABLE "Recommendation" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "user_id" int NOT NULL,
-  "movie_id" int,
-  "tv_show_id" int,
-  "watched" boolean DEFAULT false,
-  "liked" boolean DEFAULT null,
-  "date_liked" timestamp
+  "name" string,
+  "API_used" string NOT NULL,
+  "API_id" string NOT NULL
 );
 
 CREATE TABLE "Queue" (
@@ -94,35 +82,27 @@ CREATE TABLE "User_Photos" (
   "user_id" int NOT NULL
 );
 
-CREATE TABLE "User_Social_Networks" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "social_network_id" int NOT NULL,
-  "user_id" int NOT NULL
-);
-
-CREATE TABLE "Social_Networks" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "name" text NOT NULL,
-  "url" text NOT NULL,
-  "details" text
-);
-
 CREATE TABLE "Genres" (
   "id" SERIAL PRIMARY KEY NOT NULL,
-  "name" text NOT NULL,
-  "details" text
+  "name" string NOT NULL,
+  "details" string
 );
 
-CREATE TABLE "User_Movies_Watched" (
-  "id" SERIAL PRIMARY KEY,
+CREATE TABLE "Friends" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
   "user_id" int NOT NULL,
-  "movie_id" int
+  "friend_user_id" int NOT NULL,
+  "date_start" date NOT NULL
 );
 
-CREATE TABLE "User_TVShows_Watched" (
-  "id" SERIAL PRIMARY KEY,
-  "user_id" int NOT NULL,
-  "tv_show_id" int
+CREATE TABLE "Likes" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "user_id" int,
+  "liked" boolean,
+  "watched" boolean,
+  "movie_id" int,
+  "tv_show_id" int,
+  "date_liked" timestamp NOT NULL
 );
 
 ALTER TABLE "Users" ADD FOREIGN KEY ("genres_liked_id") REFERENCES "Genres" ("id");
@@ -139,30 +119,22 @@ ALTER TABLE "Country" ADD FOREIGN KEY ("state_id") REFERENCES "State" ("id");
 
 ALTER TABLE "Country" ADD FOREIGN KEY ("province_id") REFERENCES "Province" ("id");
 
-ALTER TABLE "Recommendation" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id");
-
-ALTER TABLE "Recommendation" ADD FOREIGN KEY ("movie_id") REFERENCES "Movies" ("movie_id");
-
-ALTER TABLE "Recommendation" ADD FOREIGN KEY ("tv_show_id") REFERENCES "TV_Shows" ("id");
-
 ALTER TABLE "Queue" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id");
 
-ALTER TABLE "Queue" ADD FOREIGN KEY ("movie_id") REFERENCES "Movies" ("movie_id");
+ALTER TABLE "Queue" ADD FOREIGN KEY ("movie_id") REFERENCES "Movies" ("id");
 
 ALTER TABLE "Queue" ADD FOREIGN KEY ("tv_show_id") REFERENCES "TV_Shows" ("id");
 
 ALTER TABLE "User_Photos" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id");
 
-ALTER TABLE "User_Social_Networks" ADD FOREIGN KEY ("social_network_id") REFERENCES "Social_Networks" ("id");
+ALTER TABLE "Friends" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id");
 
-ALTER TABLE "User_Social_Networks" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id");
+ALTER TABLE "Friends" ADD FOREIGN KEY ("friend_user_id") REFERENCES "Users" ("user_id");
 
-ALTER TABLE "User_Movies_Watched" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id");
+ALTER TABLE "Likes" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id");
 
-ALTER TABLE "User_Movies_Watched" ADD FOREIGN KEY ("movie_id") REFERENCES "Movies" ("movie_id");
+ALTER TABLE "Likes" ADD FOREIGN KEY ("movie_id") REFERENCES "Movies" ("id");
 
-ALTER TABLE "User_TVShows_Watched" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id");
-
-ALTER TABLE "User_TVShows_Watched" ADD FOREIGN KEY ("tv_show_id") REFERENCES "TV_Shows" ("id");
+ALTER TABLE "Likes" ADD FOREIGN KEY ("tv_show_id") REFERENCES "TV_Shows" ("id");
 
 COMMENT ON COLUMN "Authentication"."created_at" IS 'when pasword and username created';
