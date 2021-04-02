c1c9672389f88241571a507f5d7a0b6fe309ea8d
Marlon M <marlon.j.mariani@gmail.com>
2021-04-02 00:41:15 -0400

    make it better

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from models import *
 CURR_USER_KEY = "curr_user"
 
 
-
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
@@ -30,8 +29,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "this really needs to be changed")
-app.run(use_reloader=True)
-toolbar = DebugToolbarExtension(app)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+##app.run(use_reloader=True)
+##toolbar = DebugToolbarExtension(app)
 
 
 bootstrap = Bootstrap(app)
